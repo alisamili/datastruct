9322d1594b4870197df04cf97f0d9a71a357caa9
Alisa mili <Alisa_mili@126.com>
2016-08-14 15:56:34 +0800

    初始化

---
+++ datastruct/datastruct/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  datastruct
+//
+//  Created by Alisa on 16/8/14.
+//  Copyright © 2016年 null. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    printf("test");
+    return 0;
+}
